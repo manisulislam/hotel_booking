fbaa0288ddd3157088c17338308c9cf5fa914ef5
anisul islam <anisulislam980340@gmail.com>
2024-08-02 23:03:55 +0600

    static files update

--- hotel_management/urls.py
+++ hotel_management/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path,include
 from . import views
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home,name='home'),
@@ -11,6 +11,5 @@ urlpatterns = [
     path('hotel/',include('hotel.urls')),
     path('transaction/',include('transaction.urls'))
 ]
-urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
-urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
